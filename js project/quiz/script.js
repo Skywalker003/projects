99bd5b9c249d8f502f83358a85dbf3291522b183
Skywalker003 <srivikasramadas@gmail.com>
2024-12-28 22:11:05 +0530

    added

--- js project/quiz/script.js
+++ js project/quiz/script.js
@@ -1,15 +1,22 @@
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
-const mian = document.querySelector('.main');
+const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
+const quizSelection = document.querySelector('.quiz-selection');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
-    mian.classList.add('active');
+    main.classList.add('active');
 }
 
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active');
-    mian.classList.remove('active')
+    main.classList.remove('active')
+}
+
+continueBtn.onclick = () => {
+    quizSelection.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active')
 }
