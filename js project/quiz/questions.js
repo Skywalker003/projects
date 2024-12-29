182bf6d4c80960ca8a87c1fe55fc857457281252
Skywalker003 <srivikasramadas@gmail.com>
2024-12-29 15:34:22 +0530

    added

---
+++ js project/quiz/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "A) Hyperlinks and Text Markup Language",
+            "B) Hyper Text Markup Language",
+            "C) Home Tool Markup Language",
+            "D) Hyper Text Markdown Language"
+        ],
+        correct: 1
+    },
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "A) Cascading Style Sheets",
+            "B) Creative Style Sheets",
+            "C) Computer Style Sheets",
+            "D) Colorful Style Sheets"
+        ],
+        correct: 0
+    },
+    {
+        question: "Which programming language is used to create dynamic content on web pages?",
+        options: [
+            "A) Python",
+            "B) Java",
+            "C) JavaScript",
+            "D) C++"
+        ],
+        correct: 2
+    },
+    {
+        question: "What is the purpose of the <head> tag in an HTML document?",
+        options: [
+            "A) To define the main content of the document",
+            "B) To store metadata and links to stylesheets or scripts",
+            "C) To display the document title on the webpage",
+            "D) To define the footer of the document"
+        ],
+        correct: 1
+    },
+    {
+        question: "What is the correct syntax to link an external CSS file in HTML?",
+        options: [
+            "A) <style src='styles.css'>",
+            "B) <link href='styles.css' rel='stylesheet'>",
+            "C) <stylesheet>styles.css</stylesheet>",
+            "D) <css>styles.css</css>"
+        ],
+        correct: 1
+    }
+];
--- js project/quiz/script.js
+++ js project/quiz/script.js
@@ -1,9 +1,18 @@
+import {questions} from './questions'
+
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
 const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSelection = document.querySelector('.quiz-selection');
+//quiz part
+const questionE1 = document.querySelector('.question');
+const optionsE1 = document.querySelector('.options');
+const nextBtn = document.querySelector('.next-btn');
+const scoreE1 = document.getElementById('.score');
+const footerE1 = document.getElementById('.qus-no')
+
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -19,4 +28,17 @@ continueBtn.onclick = () => {
     quizSelection.classList.add('active');
     popupInfo.classList.remove('active');
     main.classList.remove('active')
+}
+
+//quiz part
+
+//quiz state 
+let currentQuestionIndex = 0;
+let score = 0;
+
+//loading qus
+
+function loadQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    
 }
