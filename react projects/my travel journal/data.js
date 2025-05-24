f5270aa69ef86cea841697ac73e2a9afab1e6c4c
Skywalker003 <srivikasramadas@gmail.com>
2025-05-24 16:03:30 +0530

    added

---
+++ react projects/my travel journal/data.js
@@ -0,0 +1,194 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-japan-image-url",
+            alt: "Mount Fuji"
+        },
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-australia-image-url",
+            alt: "Sydney Opera House"
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://scrimba.com/links/travel-journal-norway-image-url",
+            alt: "Geirangerfjord"
+        },
+        title: "Geirangerfjord",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    },
+    {
+    id: 4,
+    img: {
+        src: "https://images.unsplash.com/photo-1523906921802-b5d2d899e93b?q=80&w=2153&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Venice"
+    },
+    title: "Venice",
+    country: "Italy",
+    googleMapsLink: "https://maps.app.goo.gl/ZYHfaituDTyk8YQp9",
+    dates: "10 Apr, 2022 - 20 Apr, 2022",
+    text: "Venice is a city in northeastern Italy known for its beautiful canals, architecture, and artwork. It is built on more than 100 small islands in a lagoon in the Adriatic Sea."
+},
+{
+    id: 5,
+    img: {
+        src: "https://images.unsplash.com/photo-1568322445389-f64ac2515020?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8UHlyYW1pZHMlMjBvZiUyMEdpemF8ZW58MHx8MHx8fDA%3D",
+        alt: "Pyramids of Giza"
+    },
+    title: "Giza",
+    country: "Egypt",
+    googleMapsLink: "https://maps.app.goo.gl/9a9pnN2zSPbT5E576",
+    dates: "15 Feb, 2023 - 02 Mar, 2023",
+    text: "Giza is home to Egypt’s most iconic monuments, including the Great Pyramid and the Sphinx. These wonders are a testament to the ancient Egyptian civilization."
+},
+{
+    id: 6,
+    img: {
+        src: "https://images.unsplash.com/photo-1655642272518-05aaf7b8726e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q2hyaXN0JTIwdGhlJTIwUmVkZWVtZXJ8ZW58MHx8MHx8fDA%3D",
+        alt: "Christ the Redeemer"
+    },
+    title: "Rio de Janeiro",
+    country: "Brazil",
+    googleMapsLink: "https://maps.app.goo.gl/h6TWEtxwp9iBPEZN7",
+    dates: "20 Sep, 2022 - 04 Oct, 2022",
+    text: "Rio de Janeiro is a sprawling Brazilian city known for its beaches, the massive Christ the Redeemer statue atop Mount Corcovado, and its vibrant Carnival festival."
+},
+{
+    id: 7,
+    img: {
+        src: "https://plus.unsplash.com/premium_photo-1661940254003-c3f37e5d32ad?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Qmx1ZSUyMExhZ29vbnxlbnwwfHwwfHx8MA%3D%3D",
+        alt: "Blue Lagoon"
+    },
+    title: "Blue Lagoon",
+    country: "Iceland",
+    googleMapsLink: "https://maps.app.goo.gl/8LokydPQtRbi7JGa6",
+    dates: "01 Jun, 2022 - 12 Jun, 2022",
+    text: "The Blue Lagoon is a geothermal spa in southwestern Iceland. It's known for its steamy mineral-rich waters, surrounded by lava fields."
+},
+{
+    id: 8,
+    img: {
+        src: "https://images.unsplash.com/photo-1664387518989-bb1d8574f078?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fE1hY2h1JTIwUGljY2h1fGVufDB8fDB8fHww",
+        alt: "Machu Picchu"
+    },
+    title: "Machu Picchu",
+    country: "Peru",
+    googleMapsLink: "https://maps.app.goo.gl/Hwyq3VpQHWUHt2ut9",
+    dates: "15 Jul, 2022 - 25 Jul, 2022",
+    text: "Machu Picchu is an ancient Incan city set high in the Andes Mountains. It’s renowned for its sophisticated dry-stone walls and panoramic views."
+},
+{
+    id: 9,
+    img: {
+        src: "https://images.unsplash.com/photo-1623300973992-54b05851941d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8R3JhbmQlMjBDYW55b258ZW58MHwxfDB8fHww",
+        alt: "Grand Canyon"
+    },
+    title: "Grand Canyon",
+    country: "USA",
+    googleMapsLink: "https://maps.app.goo.gl/SuRtnkSscRVdza3aA",
+    dates: "01 May, 2022 - 10 May, 2022",
+    text: "The Grand Canyon is a massive gorge in Arizona carved by the Colorado River, known for its layered red rock formations and stunning vistas."
+},
+{
+    id: 10,
+    img: {
+        src: "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8RWlmZmVsJTIwVG93ZXJ8ZW58MHwxfDB8fHww",
+        alt: "Eiffel Tower"
+    },
+    title: "Paris",
+    country: "France",
+    googleMapsLink: "https://maps.app.goo.gl/6GY1uKmx3BWdMz4CA",
+    dates: "03 Aug, 2022 - 12 Aug, 2022",
+    text: "Paris, the capital of France, is famed for its art, fashion, and culture. The Eiffel Tower is one of the most recognizable landmarks in the world."
+},
+{
+    id: 11,
+    img: {
+        src: "https://images.unsplash.com/photo-1631646109248-a7264aae1790?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TW91bnQlMjBLaWxpbWFuamFyb3xlbnwwfDF8MHx8fDA%3D",
+        alt: "Mount Kilimanjaro"
+    },
+    title: "Mount Kilimanjaro",
+    country: "Tanzania",
+    googleMapsLink: "https://maps.app.goo.gl/7n6Z4kbuiYPzVZh17",
+    dates: "10 Sep, 2022 - 22 Sep, 2022",
+    text: "Mount Kilimanjaro is Africa’s tallest peak and the world’s tallest free-standing mountain, attracting trekkers and climbers from around the globe."
+},
+{
+    id: 12,
+    img: {
+        src: "https://images.unsplash.com/photo-1526711657229-e7e080ed7aa1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8VGFqJTIwTWFoYWx8ZW58MHwxfDB8fHww",
+        alt: "Taj Mahal"
+    },
+    title: "Taj Mahal",
+    country: "India",
+    googleMapsLink: "https://maps.app.goo.gl/Nh5crUsVm3SkgBzK8",
+    dates: "10 Dec, 2022 - 12 Dec, 2022",
+    text: "The Taj Mahal in Agra is one of the most iconic monuments in the world, known for its stunning white marble architecture and historical significance as a symbol of love."
+},
+{
+    id: 13,
+    img: {
+        src: "https://plus.unsplash.com/premium_photo-1697729634472-bb82561ef296?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8VmFyYW5hc2klMjBHaGF0c3xlbnwwfDF8MHx8fDA%3D",
+        alt: "Varanasi Ghats"
+    },
+    title: "Varanasi",
+    country: "India",
+    googleMapsLink: "https://maps.app.goo.gl/RTnDSe7EmPcuLYma9",
+    dates: "05 Jan, 2023 - 09 Jan, 2023",
+    text: "Varanasi is one of the world's oldest continuously inhabited cities, located on the banks of the Ganges River. It is a spiritual center for Hindus and known for its sacred ghats."
+},
+{
+    id: 14,
+    img: {
+        src: "https://images.unsplash.com/photo-1650530777057-3a7dbc24bf6c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8SGF3YSUyME1haGFsfGVufDB8MXwwfHx8MA%3D%3D",
+        alt: "Hawa Mahal"
+    },
+    title: "Jaipur",
+    country: "India",
+    googleMapsLink: "https://maps.app.goo.gl/X7VZLLN4PXFrzTHy7",
+    dates: "15 Feb, 2023 - 20 Feb, 2023",
+    text: "Known as the Pink City, Jaipur is famous for its historic palaces and forts. Hawa Mahal, or the Palace of Winds, is a stunning example of Rajput architecture."
+},
+{
+    id: 15,
+    img: {
+        src: "https://images.unsplash.com/photo-1735724407103-e4f42a7a5dd2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fEtlcmFsYSUyMEJhY2t3YXRlcnN8ZW58MHwxfDB8fHww",
+        alt: "Kerala Backwaters"
+    },
+    title: "Alleppey (Alappuzha)",
+    country: "India",
+    googleMapsLink: "https://maps.app.goo.gl/PUYJMCCTgG6ve58S9",
+    dates: "02 Mar, 2023 - 07 Mar, 2023",
+    text: "Alleppey, often referred to as the 'Venice of the East', is famous for its houseboat cruises through the serene Kerala backwaters and lush greenery."
+},
+{
+    id: 16,
+    img: {
+        src: "https://images.unsplash.com/photo-1730620776114-876458fdbf2e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fEdvbGRlbiUyMFRlbXBsZXxlbnwwfDF8MHx8fDA%3D",
+        alt: "Golden Temple"
+    },
+    title: "Amritsar",
+    country: "India",
+    googleMapsLink: "https://maps.app.goo.gl/FUYBm2sdLxjSgXYFA",
+    dates: "18 Apr, 2023 - 22 Apr, 2023",
+    text: "The Golden Temple, also known as Harmandir Sahib, is the spiritual and cultural center for Sikhs. Located in Amritsar, it welcomes people of all faiths."
+}
+]
