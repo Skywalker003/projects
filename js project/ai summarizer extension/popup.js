09719bdbbf6282cecb77f176285818767ebdb169
Skywalker003 <srivikasramadas@gmail.com>
2025-12-10 14:09:07 +0530

    added

--- js project/ai summarizer extension/popup.js
+++ js project/ai summarizer extension/popup.js
@@ -1,11 +1,18 @@
 document.getElementById("summarize").addEventListener("click", () =>{
+    const result = document.getElementById("result");
+    result.textContent = "Extracting Text...";
 
+    chrome.tabs.query({active: true, currentWindow: true },([tab]) => {
+        chrome.tabs.sendMessage(
+            tab.id,
+            { type: "GET_ARTICLE_TEXT"},
+            ({text}) => {
+                result.textContent = text ? text.slice(0,300) + "..." : "No article text found";
+            }
+        )
+    })
 });
 
 document.getElementById("copy-btn").addEventListener("click", () =>{
-    
-});
-
-document.getElementById("result").addEventListener("click", () =>{
     
 });
