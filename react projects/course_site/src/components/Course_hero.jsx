3a38017122e069209394d9607ca3f38738bbdbe4
SRI VIKAS S R <srivikasramadas@gmail.com>
2026-03-12 11:29:54 +0530

    added

--- react projects/course_site/src/components/Course_hero.jsx
+++ react projects/course_site/src/components/Course_hero.jsx
@@ -4,11 +4,13 @@ export default function Course_hero({ courseCount, loading, error }) {
 
   return (
     <div className="course-hero-container">
-        <h1>Explore <span>Web</span> Development Courses</h1>
-        <p>Browse structured courses designed to take you from beginner to full-stack developer.</p>
-        {!loading && !error && (
-            <p>{courseCount} Courses <span>Available</span></p>
-        )}
+        <div className="course-hero-text">
+            <h1 className="courses-header">Explore <span>Web</span> Development Courses</h1>
+            <p>Browse structured courses designed to take you from beginner to full-stack developer.</p>
+            {!loading && !error && (
+                <p>{courseCount} Courses <span>Available</span></p>
+            )}
+        </div>
     </div>
   )
 }
