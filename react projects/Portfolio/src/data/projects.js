1c8255b34c820ba4d6f689fef6241faf01628485
SRI VIKAS S R <srivikasramadas@gmail.com>
2026-03-20 14:06:15 +0530

    updated

--- react projects/Portfolio/src/data/projects.js
+++ react projects/Portfolio/src/data/projects.js
@@ -1,114 +1,125 @@
 export const projects = [
   {
     id: 1,
-    title: "AI Chef Recipe App",
+    title: "Webs_Dev Course Platform",
     featured: true,
     description:
-      "App that takes your ingredients and generates delicious recipes using AI.",
+      "A full-stack e-learning platform featuring modern React ui, dynamic data handling",
     image: "/projects/project1.png",
-    tags: ["React", "JavaScript"],
-    demoUrl: "https://chef-claude-recipe-maker.netlify.app/",
-    githubUrl: "https://github.com/Skywalker003/chef-claude",
+    tags: ["React", "Vite", "Express", "JWT"],
+    demoUrl: "https://web-dev-e-learning-platform.vercel.app/",
+    githubUrl: "https://github.com/Skywalker003/Web_Dev-E-Learning-platform",
   },
   {
     id: 2,
-    title: "Tenzies Dice Game",
-    featured: false,
-    description: "Goal is to roll the dice until all dice show the same number.",
-    image: "/projects/project2.png",
-    tags: ["React", "JavaScript"],
-    demoUrl: "https://tenzies-dice-game-sky.netlify.app/",
-    githubUrl: "https://github.com/Skywalker003/Tenzies_Dice_game",
-  },
-  {
-    id: 3,
     title: "Movie Catalog App",
     featured: true,
     description:
       "A movie catalog app with search and filtering capabilities.",
-    image: "/projects/project3.png",
+    image: "/projects/project2.png",
     tags: ["React", "JavaScript"],
     demoUrl: "https://movie-catalog-sky.netlify.app/",
     githubUrl:
       "https://github.com/Skywalker003/movie-catalog",
   },
   {
+    id: 3,
+    title: "AI Chef Recipe App",
+    featured: true,
+    description:
+      "App that takes your ingredients and generates delicious recipes using AI.",
+    image: "/projects/project3.png",
+    tags: ["React", "JavaScript"],
+    demoUrl: "https://chef-claude-recipe-maker.netlify.app/",
+    githubUrl: "https://github.com/Skywalker003/chef-claude",
+  },
+  {
     id: 4,
+    title: "Tenzies Dice Game",
+    featured: false,
+    description: "Goal is to roll the dice until all dice show the same number.",
+    image: "/projects/project4.png",
+    tags: ["React", "JavaScript"],
+    demoUrl: "https://tenzies-dice-game-sky.netlify.app/",
+    githubUrl: "https://github.com/Skywalker003/Tenzies_Dice_game",
+  },
+  {
+    id: 5,
     title: "ai summarizer extension",
     featured: true,
     description:
       "An AI-powered summarizer extension for browsers.",
-    image: "/projects/project4.png",
+    image: "/projects/project5.png",
     tags: ["JavaScript"],
     githubUrl:
       "https://github.com/Skywalker003/ai-summarizer-extension",
   },
   {
-    id: 5,
+    id: 6,
     title: "BrainBlitz",
     featured: false,
     description:
       "A quiz app with multiple-choice questions, instant feedback, and score tracking.",
-    image: "/projects/project5.png",
+    image: "/projects/project6.png",
     tags: ["JavaScript"],
     demoUrl: "https://sky-quiz.netlify.app/",
     githubUrl:
       "https://github.com/Skywalker003/projects/tree/main/js%20project/quiz",
   },
   {
-    id: 6,
+    id: 7,
     title: "meme-generator",
     featured: false,
     description:
       "A meme generator app that allows users to create and share custom memes.",
-    image: "/projects/project6.png",
+    image: "/projects/project7.png",
     tags: ["React", "JavaScript"],
     demoUrl: "https://meme-generator-flip.netlify.app/",
     githubUrl:
       "https://github.com/Skywalker003/MemeCraft",
   },
   {
-    id: 7,
+    id: 8,
     title: "lead tracker extension",
     featured: false,
     description:
       "A lead tracking extension for browsers.",
-    image: "/projects/project7.png",
+    image: "/projects/project8.png",
     tags: ["JavaScript"],
     githubUrl:
       "https://github.com/Skywalker003/Lead-tracker-extension",
   },
   {
-    id: 8,
+    id: 9,
     title: "Calculator App",
     featured: false,
     description:
       "A simple calculator app with basic arithmetic operations.",
-    image: "/projects/project8.png",
+    image: "/projects/project9.png",
     tags: ["JavaScript"],
     demoUrl: "https://calculator-sky.netlify.app/",
     githubUrl:
       "https://github.com/Skywalker003/projects/tree/main/js%20project/calculator",
   },
   {
-    id: 9,
+    id: 10,
     title: "Clock App",
     featured: false,
     description:
       "A clock app that displays the current time and updates in real-time.",
-    image: "/projects/project9.png",
+    image: "/projects/project10.png",
     tags: ["JavaScript"],
     demoUrl: "https://digital-clock-sky.netlify.app/",
     githubUrl:
       "https://github.com/Skywalker003/projects/tree/main/js%20project/clock",
   },
   {
-    id: 10,
+    id: 11,
     title: "Youtube Clone",
     featured: false,
     description:
       "minimilistic A YouTube clone app .",
-    image: "/projects/project10.png",
+    image: "/projects/project11.png",
     tags: ["HTML", "CSS"],
     demoUrl: "https://youtube-clone-sws.netlify.app/",
     githubUrl:
