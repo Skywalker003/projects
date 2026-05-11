2b8738934c613855bd649c0ab496fa53badee82e
SRI VIKAS S R <srivikasramadas@gmail.com>
2026-05-11 10:23:00 +0530

    updated about page

--- react projects/kalanjiyum/src/components/sections/about/About_Hero.jsx
+++ react projects/kalanjiyum/src/components/sections/about/About_Hero.jsx
@@ -6,7 +6,7 @@ export default function About_Hero() {
         <PageHero
             className="about-hero"
             page="About"
-            title="About Kalanjiyam Tech"
+            title={<>About<br />Kalanjiyam <span className="page-hero_title--accent">Tech</span></>}
             subtext="A passionate team delivering cost-effective technology solutions since 2019."
         />
     )
